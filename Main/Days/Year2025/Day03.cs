14b786cb5ce637ef393274cd2b4f866a7eba3e01
Till Knollmann <till-knollmann@outlook.de>
2025-12-03 22:48:03 +0100

    :sparkles: add solution to day 3

---
+++ Main/Days/Year2025/Day03.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode.Days.Year2025;
+
+internal class Day03 : DayOfYear2025<Day03, List<Day03.Bank>>
+{
+    public override int Number => 3;
+
+    protected override List<Bank> ParseInput()
+    {
+        return Input.Split("\n")
+            .Select(Bank.Parse)
+            .ToList();
+    }
+
+    public override string FirstPart()
+    {
+        return ParsedInput.Value
+            .Select(bank => bank.GetHighestJolt(2))
+            .Sum()
+            .ToString();
+    }
+
+    public override string SecondPart()
+    {
+        return ParsedInput.Value
+            .Select(bank => bank.GetHighestJolt(12))
+            .Sum()
+            .ToString();
+    }
+
+    internal class Bank
+    {
+        private Bank(int[] batteries)
+        {
+            Batteries = batteries;
+        }
+
+        private int[] Batteries { get; }
+
+        public static Bank Parse(string input)
+        {
+            return new Bank(input.Trim().ToCharArray()
+                .Select(character => int.Parse(character.ToString()))
+                .ToArray());
+        }
+
+        public long GetHighestJolt(int digits)
+        {
+            int[] selectedDigits = [];
+            var lastSelectedDigitIndex = -1;
+            for (var i = 1; i <= digits; i++)
+            {
+                (lastSelectedDigitIndex, var lastSelectedDigit) = Batteries
+                    .Index()
+                    .Skip(lastSelectedDigitIndex + 1)
+                    .Take(Batteries.Length - (lastSelectedDigitIndex + 1) - (digits - i))
+                    .MaxBy(tuple => tuple.Item);
+                selectedDigits = selectedDigits.Append(lastSelectedDigit).ToArray();
+            }
+
+            return long.Parse(string.Join("", selectedDigits));
+        }
+    }
+}
